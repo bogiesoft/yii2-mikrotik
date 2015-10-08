07d5e2c3ad699893bef2dfa566e207dbdcbcf74d
unknown <rizki.device@gmail.com>
2015-10-08 12:34:18 +0800

    Update PEAR2 Packages

--- vendor/bin/roscon.php
+++ vendor/bin/roscon.php
@@ -1,1 +1,7 @@
-../pear2/net_routeros/scripts/roscon.php
+#!/usr/bin/env sh
+SRC_DIR="`pwd`"
+cd "`dirname "$0"`"
+cd "../pear2/net_routeros/scripts"
+BIN_TARGET="`pwd`/roscon.php"
+cd "$SRC_DIR"
+"$BIN_TARGET" "$@"
